6556d29cd4b93326fe3e37aabce4354393b32231
MahonriM <cripto3000z@gmail.com>
2020-08-28 04:10:24 -0500

    Add files via upload

---
+++ random.py
@@ -0,0 +1,19 @@
+﻿#Desarrollado por Mahonri Mtz.
+#Este programa es un tipo de juego en ql que tienes que adivinar un numero entre el 0 al 10
+import random
+print("Intenta adivinar un numero entre el 0 al 10")
+numero= random.randint(0,11)
+op=0
+while op<10:
+    x=int(input("ingresa tu numero"))
+    if x>numero:
+        print("El numero es mas pequeño")
+    if x<numero:
+        print("El numero es mas grande")
+    elif x==numero:
+        print("Le atinaste al numero")
+        break
+op=op+1
+
+
+
