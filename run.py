72e15ab06528bd3f4638866d72a4a428299d23b8
nsandarma <nsandarma@gmail.com>
2021-11-04 05:35:07 +0700

    adad

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
 class Siswa(db.Model):
