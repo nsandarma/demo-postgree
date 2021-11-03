71130312d4c936e0a41673c6d9d840e03101c324
nsandarma <nsandarma@gmail.com>
2021-11-04 05:23:46 +0700

    resou

--- run.py
+++ run.py
@@ -8,8 +8,8 @@ sqlite = 'sqlite:///test.db'
 secret = 'fasisme123'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = database
-app.config['SECRET_KEY'] = secret
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 db = SQLAlchemy(app)
 
 class Siswa(db.Model):
